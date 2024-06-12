ac4e9fd7e3cf0d2210c1bc6099b4822f1da28fae
YtalloAlves <ytollo.alves@academico.ifpb.edu.br>
2024-06-12 18:35:32 -0300

    :recycle: refactor: mudando rota getGameById para retornar somente os dados necessários

--- src/routes/gameRoutes/getGameById.js
+++ src/routes/gameRoutes/getGameById.js
@@ -3,8 +3,10 @@ const { api } = require('../api')
 const getGameById = async (gameId) => {
   const response = await api.get(`/games/${gameId}`)
   .then(response => {
-      console.log(response.data)
-      return response.data
+      const { name, description, developer, publisher, released_date } = response.data.data
+      const data = {name, description, developer, publisher, released_date}
+      console.log(data)
+      return data
   })
   .catch(error => {
       console.error(error)
