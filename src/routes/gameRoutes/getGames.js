5aa3828a45d624517051dd2f5b5e87633c901161
Ygor Matos <ygorsilvamatos@hotmail.com>
2024-06-12 17:50:41 -0300

    :recycle: refactor: requisitando somente dados necessários

--- src/routes/gameRoutes/getGames.js
+++ src/routes/gameRoutes/getGames.js
@@ -3,9 +3,12 @@ const { api } = require('../api')
 const getGames = async () => {
   const response = await api.get(`/games?limit=32`)
   .then(response => {
-
-      console.log(response.data)
-      return response.data
+    const games=response.data.data;
+    // games.forEach(game => {
+    //   console.log(game);
+    // });
+    // return response.data
+    console.log(games[31].name);
   })
   .catch(error => {
       console.error(error)
