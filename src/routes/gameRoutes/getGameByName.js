4a1e24fe90659c06eecc6d2576bfc98d1c19d0af
Ygor Matos <ygorsilvamatos@hotmail.com>
2024-06-13 10:28:08 -0300

    :recycle: refactor: tornando o método getGameByName funcional

--- src/routes/gameRoutes/getGameByName.js
+++ src/routes/gameRoutes/getGameByName.js
@@ -1,14 +1,16 @@
-const { api } = require('../api')
-
+const { api } = require('../api') 
+// o nome tem que estar na exata formatação da API, "Ocarina" tem retorno.
+//"ocarina" não tem.
 const getGameByName = async (gameName) => {
-  const response = await api.get(`/games/?name=${gameName}`)
-  .then(response => {
-      console.log(response.data)
-      return response.data
-  })
-  .catch(error => {
-      console.error(error)
-  })
-}
-
+  try{
+    const response = await api.get(`/games?name=${gameName}`);
+    const data = response.data.data;
+    //const data = { name, description, developer, publisher, released_date };
+    //aqui colocar um loop de for each rodando e exibindo cada um dos atributos 
+    return data;
+  } catch (error){
+    //console.error(error);
+    throw error;
+  }
+};
 module.exports = { getGameByName }
