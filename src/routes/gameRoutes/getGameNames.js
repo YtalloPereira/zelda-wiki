daa615fd28c3f503a62634b51f98e20c39c19bd6
Ygor Matos <ygorsilvamatos@hotmail.com>
2024-06-15 16:06:39 -0300

    :recycle: refactor: eliminando jogos repetidos

--- src/routes/gameRoutes/getGameNames.js
+++ src/routes/gameRoutes/getGameNames.js
@@ -6,8 +6,8 @@ const getGameNames = async (req, res) => {
   try {
     const response = await api.get(`/games?limit=32`);
     const games = response.data.data.map(game => game.name); // Extrai apenas os nomes dos jogos
-    console.log(games);
-    res.status(200).json(games); // Retorna os nomes dos jogos como JSON com status 200
+    const gamesFinal = [...new Set(games)]; // Removendo jogo repetido
+    res.status(200).json(gamesFinal); // Retorna os nomes dos jogos como JSON com status 200
   } catch (error) {
     console.error('Erro ao obter os nomes dos jogos:', error);
     res.status(500).json({ error: 'Erro ao obter os nomes dos jogos' }); // Retorna um erro interno do servidor com status 500
